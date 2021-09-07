8abf21aa5c0fd6cf6d075c0e6031d4cc23203e0a
Dimitar Ivanov <didesignbg@yahoo.com>
2021-09-07 16:18:20 +0000

    Update xdebug config to v3

--- images/php/7.4/fpm/start.sh
+++ images/php/7.4/fpm/start.sh
@@ -2,12 +2,10 @@
 XDEBUG_CONFIG_FILE=/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 if [ "${XDEBUG_IDEKEY}" = "PHPSTORM" ]
 then
-    sed -i '1 a xdebug.remote_mode=req' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.remote_handler=dbgp' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.remote_connect_back=1 ' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.remote_port=${XDEBUG_REMOTE_PORT}' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.remote_enable=1' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.remote_log=/tmp/xdebug.log' $XDEBUG_CONFIG_FILE
+    sed -i '1 a xdebug.mode=develop,debug,trace' $XDEBUG_CONFIG_FILE
+    sed -i '1 a xdebug.client_port=${XDEBUG_REMOTE_PORT}' $XDEBUG_CONFIG_FILE
+    sed -i '1 a xdebug.dicsover_client_host=true' $XDEBUG_CONFIG_FILE
+    sed -i '1 a xdebug.log=/tmp/xdebug.log' $XDEBUG_CONFIG_FILE
     sed -i '1 a xdebug.idekey=${XDEBUG_IDEKEY}' $XDEBUG_CONFIG_FILE
 else
     if ! grep -Fq ";zend_extension" $XDEBUG_CONFIG_FILE
