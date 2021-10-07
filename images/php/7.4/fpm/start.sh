9a97fc9616f969e87d22facf1cec60f108a9e615
Dimitar Ivanov <didesignbg@yahoo.com>
2021-10-07 15:20:44 +0000

    Fix xdebug add pcntl

--- images/php/7.4/fpm/start.sh
+++ images/php/7.4/fpm/start.sh
@@ -4,7 +4,7 @@ if [ "${XDEBUG_IDEKEY}" = "PHPSTORM" ]
 then
     sed -i '1 a xdebug.mode=develop,debug,trace' $XDEBUG_CONFIG_FILE
     sed -i '1 a xdebug.client_port=${XDEBUG_REMOTE_PORT}' $XDEBUG_CONFIG_FILE
-    sed -i '1 a xdebug.dicsover_client_host=true' $XDEBUG_CONFIG_FILE
+    sed -i '1 a xdebug.discover_client_host=true' $XDEBUG_CONFIG_FILE
     sed -i '1 a xdebug.log=/tmp/xdebug.log' $XDEBUG_CONFIG_FILE
     sed -i '1 a xdebug.idekey=${XDEBUG_IDEKEY}' $XDEBUG_CONFIG_FILE
 else
